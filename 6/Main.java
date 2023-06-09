8575729b38912026aed939d017cdeab5852ec948
Fadhil <114307570+Fadhilkhan04@users.noreply.github.com>
2023-06-09 12:33:40 +0530

    packages

--- 6/Main.java
+++ 6/Main.java
@@ -1,1 +1,9 @@
+import me.fadhil;
+
+public class Main {
+  public static void main(String args[]) {
+    fadhil obj= new fadhil();
+    obj.msg();
+  }
+};
 
---
+++ 6/fadhil.java
@@ -0,0 +1,6 @@
+package me;
+public class fadhil{
+    public void msg() {
+        System.out.println("Hello");
+    }
+};
